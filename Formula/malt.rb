0159b39b80ac8421dc40c17da3e915bb0f2fc98b
Akihito Koriyama <akihito.koriyama@gmail.com>
2025-03-11 10:42:57 +0900

    Update malt formula to version 1.0.0beta2
    Bump the malt formula version from 1.0.0beta1 to 1.0.0beta2. This ensures the formula is up-to-date with the latest release. No other changes were made to the formula.

--- Formula/malt.rb
+++ Formula/malt.rb
@@ -1,7 +1,7 @@
 class Malt < Formula
   desc "JSON-driven Homebrew Dev Services"
   homepage "https://github.com/koriym/homebrew-malt"
-  version "1.0.0beta1"
+  version "1.0.0beta2"
   url "file:///dev/null"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
