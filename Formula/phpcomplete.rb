c7ad08747b30925025f48e1c5509df5b9823f4ee
Akihito Koriyama <akihito.koriyama@gmail.com>
2025-03-18 01:51:29 +0900

    Add PHPComplete formula and README for managing PHP versions
    This commit introduces the PHPComplete Homebrew formula, enabling the installation of multiple PHP versions (5.6 to 8.4) with XDebug and other PECL extensions. It also includes a detailed README with usage instructions, customization options, and methods for switching between PHP versions using shell aliases or terminal profiles.

---
+++ Formula/phpcomplete.rb
@@ -0,0 +1,36 @@
+class Phpcomplete < Formula
+  desc "Install multiple versions of PHP with XDebug and other PECL packages"
+  homepage "https://www.php.net/"
+  version "0.1.0"
+  url "file:///dev/null"
+  sha256 ""
+  license "MIT"
+
+  # Please comment out any unnecessary PHP versions.
+  depends_on "shivammathur/php/php@5.6"
+  depends_on "shivammathur/php/php@7.0"
+  depends_on "shivammathur/php/php@7.1"
+  depends_on "shivammathur/php/php@7.2"
+  depends_on "shivammathur/php/php@7.3"
+  depends_on "shivammathur/php/php@7.4"
+  depends_on "shivammathur/php/php@8.0"
+  depends_on "shivammathur/php/php@8.1"
+  depends_on "shivammathur/php/php@8.2"
+  depends_on "shivammathur/php/php@8.3"
+  depends_on "shivammathur/php/php@8.4"
+  depends_on "shivammathur/extensions/xdebug@5.6"
+  depends_on "shivammathur/extensions/xdebug@7.0"
+  depends_on "shivammathur/extensions/xdebug@7.1"
+  depends_on "shivammathur/extensions/xdebug@7.2"
+  depends_on "shivammathur/extensions/xdebug@7.3"
+  depends_on "shivammathur/extensions/xdebug@7.4"
+  depends_on "shivammathur/extensions/xdebug@8.0"
+  depends_on "shivammathur/extensions/xdebug@8.1"
+  depends_on "shivammathur/extensions/xdebug@8.2"
+  depends_on "shivammathur/extensions/xdebug@8.3"
+  depends_on "shivammathur/extensions/xdebug@8.4"
+
+  test do
+    assert_match "phpcomplete installation successful", shell_output("cat #{prefix}/INSTALLED")
+  end
+end
