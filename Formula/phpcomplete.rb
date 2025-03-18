c643fbc91f0f9fae1325833153db256058596435
Akihito Koriyama <akihito.koriyama@gmail.com>
2025-03-18 11:19:34 +0900

    Add usage instructions for switching PHP versions
    This update introduces a `caveats` method to provide users with clear instructions on how to switch between multiple installed PHP versions. It includes examples for creating shell aliases and using version switcher commands for convenience. Additionally, a reference link is provided for more detailed guidance.

--- Formula/phpcomplete.rb
+++ Formula/phpcomplete.rb
@@ -36,6 +36,37 @@ class Phpcomplete < Formula
     (prefix/"INSTALLED").write "phpcomplete installation successful"
   end
 
+  def caveats
+    <<~EOS
+  Multiple PHP versions have been installed. To use a specific version, you can:
+
+  1. Create shell aliases (add to your .bashrc or .zshrc):
+     ```shell
+     alias php56='export PATH="/opt/homebrew/opt/php@5.6/bin:$PATH" && echo "Switched to PHP 5.6"'
+     alias php70='export PATH="/opt/homebrew/opt/php@7.0/bin:$PATH" && echo "Switched to PHP 7.0"'
+     alias php71='export PATH="/opt/homebrew/opt/php@7.1/bin:$PATH" && echo "Switched to PHP 7.1"'
+     alias php72='export PATH="/opt/homebrew/opt/php@7.2/bin:$PATH" && echo "Switched to PHP 7.2"'
+     alias php73='export PATH="/opt/homebrew/opt/php@7.3/bin:$PATH" && echo "Switched to PHP 7.3"'
+     alias php74='export PATH="/opt/homebrew/opt/php@7.4/bin:$PATH" && echo "Switched to PHP 7.4"'
+     alias php80='export PATH="/opt/homebrew/opt/php@8.0/bin:$PATH" && echo "Switched to PHP 8.0"'
+     alias php81='export PATH="/opt/homebrew/opt/php@8.1/bin:$PATH" && echo "Switched to PHP 8.1"'
+     alias php82='export PATH="/opt/homebrew/opt/php@8.2/bin:$PATH" && echo "Switched to PHP 8.2"'
+     alias php83='export PATH="/opt/homebrew/opt/php@8.3/bin:$PATH" && echo "Switched to PHP 8.3"'
+     alias php84='export PATH="/opt/homebrew/opt/php@8.4/bin:$PATH" && echo "Switched to PHP 8.4"'
+     ```
+
+  2. Use version switcher commands:
+     php8.2
+     php -v   # Now using PHP 8.2
+     
+     php7.4
+     php -v   # Now using PHP 7.4
+
+  For more detailed instructions, see:
+  https://github.com/koriym/homebrew-malt/phpcpmplete/
+  EOS
+  end
+
   test do
     assert_match "phpcomplete installation successful", shell_output("cat #{prefix}/INSTALLED")
   end
